919adabaefbcac84a9c81aef4fbeac67fdd6041a
Dinis Cruz <dinis.cruz@owasp.org>
2024-12-04 23:41:22 +0000

    wired up routes to fastapi

--- cbr_custom_news_feeds/fast_api/routes/Routes__Hacker_News.py
+++ cbr_custom_news_feeds/fast_api/routes/Routes__Hacker_News.py
@@ -1,7 +1,10 @@
 from osbot_fast_api.api.Fast_API_Routes                                                   import Fast_API_Routes
 from cbr_custom_news_feeds.providers.cyber_security.hacker_news.Hacker_News__Http_Content import Hacker_News__Http_Content
 
-ROUTES_PATHS__HACKER_NEWS = [ '/hacker-news/feed']
+ROUTES_PATHS__HACKER_NEWS = [ '/hacker-news/feed'            ,
+                              '/hacker-news/prompt-analysis' ,
+                              '/hacker-news/prompt-schema'   ,
+                              '/hacker-news/prompt-executive']
 
 
 class Routes__Hacker_News(Fast_API_Routes):
@@ -11,6 +14,17 @@ class Routes__Hacker_News(Fast_API_Routes):
     def feed(self):                                         # Get the complete feed data
         return self.http_content.get_feed_data()
 
+    def prompt_analysis(self, size=5):
+        return self.http_content.get_prompt_analysis(size=size)
+
+    def prompt_schema(self, size=5):
+        return self.http_content.get_prompt_schema(size=size)
+
+    def prompt_executive(self, size=5):
+        return self.http_content.get_prompt_executive(size=size)
 
     def setup_routes(self):
-        self.add_route_get(self.feed)
+        self.add_route_get(self.feed                )
+        self.add_route_get(self.prompt_analysis )
+        self.add_route_get(self.prompt_schema   )
+        self.add_route_get(self.prompt_executive)
