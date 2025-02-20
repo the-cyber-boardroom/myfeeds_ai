bd06e8893f0af3eb173142076d122cf2d1285459
Dinis Cruz <dinis.cruz@owasp.org>
2025-02-20 23:59:59 +0000

    improved logic of Flow__Hacker_News__Process_New_Articles

--- myfeeds_ai/providers/cyber_security/hacker_news/flows/Flow__Hacker_News__Process_New_Articles.py
+++ myfeeds_ai/providers/cyber_security/hacker_news/flows/Flow__Hacker_News__Process_New_Articles.py
@@ -1,11 +1,62 @@
+from mgraph_db.mgraph.actions.MGraph__Diff import MGraph__Diff
+
+from mgraph_db.mgraph.schemas.Schema__MGraph__Diff import Schema__MGraph__Diff
+
+from mgraph_db.providers.time_chain.MGraph__Time_Chain import MGraph__Time_Chain
+from osbot_utils.context_managers.capture_duration import capture_duration
+from osbot_utils.context_managers.print_duration import print_duration
 from osbot_utils.helpers.flows.Flow             import Flow
 from osbot_utils.helpers.flows.decorators.flow  import flow
+from osbot_utils.helpers.flows.decorators.task import task
 from osbot_utils.type_safe.Type_Safe            import Type_Safe
+from osbot_utils.utils.Dev import pprint
+from osbot_utils.utils.Http import GET, GET_json
 
+MY_FEEDS__SERVER              = 'https://dev.myfeeds.ai'
+WEB_PATH__PUBLIC__HACKER_NEWS = "public-data/hacker-news"
+
+class Schema__Feeds_New_Articles(Type_Safe):
+    path__timeline__current     : str                  = None
+    path__timeline__previous    : str                  = None
+    timeline_diff               : Schema__MGraph__Diff = None
 
 class Flow__Hacker_News__Process_New_Articles(Type_Safe):
-    output : dict
+    output                      : dict
+    new_articles                : Schema__Feeds_New_Articles
+    path__timeline__current     : str = None
+    path__timeline__previous    : str = None
+    mgraph__diff                : MGraph__Diff
+    mgraph__timeline__current   : MGraph__Time_Chain          = None
+    mgraph__timeline__previous  : MGraph__Time_Chain          = None
+    duration__load_timeline_data: float
+    durations                   : dict
+    timeline_diff               : Schema__MGraph__Diff
 
+    @task()
+    def load_and_diff_timeline_data(self):
+        with self.new_articles as _:
+            if _.path__timeline__current is None:
+                raise ValueError("in load_timeline_data, the new_articles.path__timeline__current was not set")
+            if _.path__timeline__previous is None:
+                raise ValueError("in load_timeline_data, the new_articles.path__timeline__previous was not set")
+
+            url__timeline_current   = f"{MY_FEEDS__SERVER}/{WEB_PATH__PUBLIC__HACKER_NEWS}/{_.path__timeline__current }"
+            url__timeline_previous  = f"{MY_FEEDS__SERVER}/{WEB_PATH__PUBLIC__HACKER_NEWS}/{_.path__timeline__previous}"
+            with capture_duration() as duration:
+                data__timeline_current  = GET_json(url__timeline_current)
+                data__timeline_previous = GET_json(url__timeline_previous)
+                self.mgraph__timeline__current  = MGraph__Time_Chain.from_json__compressed(data__timeline_current )
+                self.mgraph__timeline__previous = MGraph__Time_Chain.from_json__compressed(data__timeline_previous)
+                self.mgraph__diff               = MGraph__Diff(graph_a = self.mgraph__timeline__current.graph ,
+                                                               graph_b = self.mgraph__timeline__previous.graph)
+                self.timeline_diff              = self.mgraph__diff.diff_graphs()
+
+        self.duration__load_timeline_data = duration.seconds
+
+    @task()
+    def process_diff(self):
+        print("process diff will go here")
+    @task()
     def create_output(self):
         return {}
 
@@ -13,6 +64,7 @@ class Flow__Hacker_News__Process_New_Articles(Type_Safe):
     @flow()
     def process_rss(self) -> Flow:
         with self as _:
+            _.load_and_diff_timeline_data()
             # _.fetch_rss_feed()
             # _.create_timeline()
             # _.save_timeline()
--- tests/integration/providers/hacker_news/flows/test__int__Flow__Hacker_News__Process_New_Articles.py
+++ tests/integration/providers/hacker_news/flows/test__int__Flow__Hacker_News__Process_New_Articles.py
@@ -1,7 +1,10 @@
 from unittest import TestCase
 
+from mgraph_db.mgraph.actions.MGraph__Diff import MGraph__Diff
+
 from myfeeds_ai.providers.cyber_security.hacker_news.flows.Flow__Hacker_News__Process_New_Articles import \
     Flow__Hacker_News__Process_New_Articles
+from osbot_utils.helpers.flows.decorators.flow import flow
 from osbot_utils.utils.Dev import pprint
 
 
@@ -10,7 +13,20 @@ class test_Flow__Hacker_News__Process_New_Articles(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.flow_process_new_articles = Flow__Hacker_News__Process_New_Articles()
+        cls.path__timeline__current   = '2025/02/20/23/feed-timeline.mgraph.json'
+        cls.path__timeline__previous  = '2025/02/19/22/feed-timeline.mgraph.json'
+        with cls.flow_process_new_articles as _:
+            _.new_articles.path__timeline__current  = cls.path__timeline__current
+            _.new_articles.path__timeline__previous = cls.path__timeline__previous
 
     def test_process_flow(self):
-        result                   = self.flow_process_new_articles.run()
-        pprint(result.flow_return_value)
+        with self.flow_process_new_articles as _:
+            result = self.flow_process_new_articles.run()
+        #pprint(result.flow_return_value)
+
+    def test_load_and_diff_timeline_data(self):
+        with self.flow_process_new_articles as _:
+            _.load_and_diff_timeline_data()
+            assert _.timeline_diff is not None
+
+            #pprint(_.timeline_diff.json())
