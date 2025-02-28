33609dee2068ba448f8e51edd73b3acb4d553313
Dinis Cruz <dinis.cruz@owasp.org>
2025-02-28 12:53:02 +0000

    started refactoring hacker_news flows into Routes__Hacker_News__Flows

---
+++ myfeeds_ai/providers/cyber_security/hacker_news/actions/Hacker_News__Flows.py
@@ -0,0 +1,16 @@
+from myfeeds_ai.providers.cyber_security.hacker_news.actions.Hacker_News__Data import Hacker_News__Data
+from osbot_utils.type_safe.Type_Safe import Type_Safe
+from osbot_utils.utils.Lists import list_index_by, list_group_by
+
+
+class Hacker_News__Flows(Type_Safe):
+    hacker_news_data: Hacker_News__Data
+
+    def current_articles(self):
+        return self.hacker_news_data.current_articles()
+
+    def current_articles__group_by__status(self):
+        articles        = self.current_articles().json().get('articles')
+        articles_values = list(articles.values())
+
+        return list_group_by(articles_values,'status')
---
+++ tests/integration/providers/hacker_news/actions/test_Hacker_News__Flows.py
@@ -0,0 +1,34 @@
+from unittest                                                                   import TestCase
+from myfeeds_ai.providers.cyber_security.hacker_news.actions.Hacker_News__Flows import Hacker_News__Flows
+from osbot_utils.utils.Misc import list_set
+from tests.integration.data_feeds__objs_for_tests                               import cbr_website__assert_local_stack
+
+from osbot_utils.utils.Dev import pprint
+
+class test_Hacker_News__Flows(TestCase):
+
+    @classmethod                                                                       # Setup test environment
+    def setUpClass(cls):
+        cbr_website__assert_local_stack()                                              # Ensure LocalStack is running
+        cls.hacker_new__flows = Hacker_News__Flows()
+
+    def test_current_articles(self):
+        with self.hacker_new__flows as _:
+            articles = _.current_articles()
+            pprint(articles)
+
+    def test_current_articles__group_by__status(self):
+        with self.hacker_new__flows as _:
+            articles_by_status = _.current_articles__group_by__status()
+            #pprint(articles_by_status)
+            for status, articles in articles_by_status.items():
+                pprint(status)
+                for article in articles:
+                    assert list_set(article) == ['knowledge_graph',
+                                                 'llm_prompt',
+                                                 'location',
+                                                 'path__entities_mgraph__json',
+                                                 'path__entities_mgraph__png',
+                                                 'path__feed_article',
+                                                 'status']
+                    assert article.get('status') == status
