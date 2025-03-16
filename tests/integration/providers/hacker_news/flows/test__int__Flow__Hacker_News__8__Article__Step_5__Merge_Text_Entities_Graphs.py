a91c8b32f87dbb2f1b0b0ae4a9ced1f0dfc2272f
Dinis Cruz <dinis.cruz@owasp.org>
2025-03-16 14:30:40 +0000

    wired self.file_articles_current.save() in step 4 added parallel execution to flow 8

--- tests/integration/providers/hacker_news/flows/test__int__Flow__Hacker_News__8__Article__Step_5__Merge_Text_Entities_Graphs.py
+++ tests/integration/providers/hacker_news/flows/test__int__Flow__Hacker_News__8__Article__Step_5__Merge_Text_Entities_Graphs.py
@@ -2,8 +2,6 @@ from unittest                                                                   
 from myfeeds_ai.providers.cyber_security.hacker_news.flows.Flow__Hacker_News__8__Article__Step_5__Merge_Text_Entities_Graphs import Flow__Hacker_News__8__Article__Step_5__Merge_Text_Entities_Graphs
 from tests.integration.data_feeds__objs_for_tests                                                                            import myfeeds_tests__setup_local_stack
 
-from osbot_utils.utils.Dev import pprint
-
 
 class test__int__Flow__Hacker_News__8__Article__Step_5__Merge_Text_Entities_Graphs(TestCase):
 
@@ -19,12 +17,12 @@ class test__int__Flow__Hacker_News__8__Article__Step_5__Merge_Text_Entities_Grap
             _.task__1__load_articles_to_process()
             assert _.articles_to_process      == _.file_articles_current.next_step__5__merge_text_entities_graphs()
             assert len(_.articles_to_process) >=  0
-            pprint(_.articles_to_process)
 
     def test_task__2__llm__create_text_entities_graphs(self):
         with self.flow_llm_merge_text_entities_graphs as _:
+            _.max_graphs_to_merge = 4
             _.task__1__load_articles_to_process         ()
             _.task__2__llm__merge_text_entities_graphs ()
 
             # from osbot_utils.utils.Dev import pprint
-            # pprint(_.status_changes.json())
+            #pprint(_.status_changes.json())
