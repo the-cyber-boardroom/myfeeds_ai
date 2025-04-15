863db086952054040f05de4da1e16c6c1269d1be
Dinis Cruz <dinis.cruz@owasp.org>
2025-04-15 14:24:33 +0100

    skipping test that needs AWS access

--- tests/integration/personas/test_My_Feeds__Persona__Files.py
+++ tests/integration/personas/test_My_Feeds__Persona__Files.py
@@ -1,10 +1,14 @@
 from unittest                                                                   import TestCase
+
+import pytest
+
 from myfeeds_ai.personas.actions.My_Feeds__Persona__Files                       import My_Feeds__Persona__Files
 from myfeeds_ai.personas.config.Config__My_Feeds__Personas                      import FILE_ID__PERSONA, FILE_ID__PERSONA__ARTICLES__CONNECTED__ENTITIES
 from myfeeds_ai.personas.files.My_Feeds__Personas__File                         import My_Feeds__Personas__File
 from myfeeds_ai.personas.files.My_Feeds__Personas__File__Now                    import My_Feeds__Personas__File__Now
 from myfeeds_ai.personas.schemas.Schema__Persona__Articles__Connected_Entities  import Schema__Persona__Articles__Connected_Entities
 from myfeeds_ai.personas.schemas.Schema__Persona__Types                         import Schema__Persona__Types
+from osbot_utils.utils.Env import in_github_action
 from tests.integration.data_feeds__objs_for_tests                               import myfeeds_tests__setup_local_stack
 
 class test_My_Feeds__Persona__Files(TestCase):
@@ -31,6 +35,8 @@ class test_My_Feeds__Persona__Files(TestCase):
             assert _.path_now   () == f'{_.folder__path_now()}/{FILE_ID__PERSONA}.json'
 
     def test_persona__description__png(self):
+        if in_github_action():
+            pytest.skip("Test needs aws access")
         with self.persona_files as _:
             persona_type = Schema__Persona__Types.EXEC__CISO
             png_bytes = _.persona__description__png(persona_type)
