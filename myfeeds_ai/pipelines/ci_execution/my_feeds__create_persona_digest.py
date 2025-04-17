35e34eea726ef01d9a0b013c2413f0b47d274f25
Dinis Cruz <dinis.cruz@owasp.org>
2025-04-17 17:42:04 +0100

    added support for passing the persona type via GH action

--- myfeeds_ai/pipelines/ci_execution/my_feeds__create_persona_digest.py
+++ myfeeds_ai/pipelines/ci_execution/my_feeds__create_persona_digest.py
@@ -1,3 +1,4 @@
+import sys
 from myfeeds_ai.personas.schemas.Schema__Persona__Types                         import Schema__Persona__Types
 from myfeeds_ai.pipelines.flows.Pipeline__Hacker_News__Create_Persona_Digest    import Pipeline__Hacker_News__Create_Persona_Digest
 from osbot_utils.utils.Dev                                                      import pprint
@@ -6,7 +7,14 @@ from osbot_utils.utils.Dev                                                      
 
 if __name__ == '__main__':
 
-    persona_type = Schema__Persona__Types.EXEC__CISO                                     # todo: this needs to be passed as a param
+    input_arg = sys.argv[1] if len(sys.argv) > 1 else 'EXEC__CISO'
+
+    try:
+        persona_type = Schema__Persona__Types[input_arg]
+    except KeyError:
+        raise ValueError(f"Invalid persona type: {input_arg}. Must be one of: {list(Schema__Persona__Types.__members__.keys())}")
+
+    #persona_type = Schema__Persona__Types.EXEC__CISO                                     # todo: this needs to be passed as a param
     with Pipeline__Hacker_News__Create_Persona_Digest(persona_type=persona_type) as _:
         flow         = _.create_persona_digest()
 
