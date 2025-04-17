4b7759db480dcc2432e512351566f416d979662b
Dinis Cruz <dinis.cruz@owasp.org>
2025-04-17 17:18:04 +0100

    added AWS creds for create persona digest

--- myfeeds_ai/pipelines/ci_execution/my_feeds__create_persona_digest.py
+++ myfeeds_ai/pipelines/ci_execution/my_feeds__create_persona_digest.py
@@ -1,8 +1,7 @@
 from myfeeds_ai.pipelines.flows.Pipeline__Hacker_News__Create_Persona_Digest    import Pipeline__Hacker_News__Create_Persona_Digest
-from tests.integration.data_feeds__objs_for_tests                               import myfeeds_tests__setup_local_stack
 
 if __name__ == '__main__':
-    myfeeds_tests__setup_local_stack()
+    #myfeeds_tests__setup_local_stack()
     with Pipeline__Hacker_News__Create_Persona_Digest() as _:
         flow = _.create_persona_digest()
         flow.flow_config.log_to_console = True
