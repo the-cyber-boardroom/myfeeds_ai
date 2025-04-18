7028318e265cccb6b630468215053ec0cd759251
Dinis Cruz <dinis.cruz@owasp.org>
2025-04-18 13:15:40 +0100

    wired up pipline_1_create_digest in Routes__My_Feeds__Personas__Admin

--- myfeeds_ai/pipelines/ci_execution/my_feeds__create_persona_digest.py
+++ myfeeds_ai/pipelines/ci_execution/my_feeds__create_persona_digest.py
@@ -1,14 +1,18 @@
 import sys
+from pprint                                                                     import pprint
 from myfeeds_ai.personas.schemas.Schema__Persona__Types                         import Schema__Persona__Types
 from myfeeds_ai.pipelines.flows.Pipeline__Hacker_News__Create_Persona_Digest    import Pipeline__Hacker_News__Create_Persona_Digest
-from osbot_utils.utils.Dev                                                      import pprint
+
 
 #myfeeds_tests__setup_local_stack()
+
+DEFAULT_PERSONA = 'EXEC__CISO'
+#DEFAULT_PERSONA = 'TEAM__APP_SEC'           # was failing here
 
 if __name__ == '__main__':
 
     raw_arg   = sys.argv[1] if len(sys.argv) > 1 else ''
-    input_arg = raw_arg.strip() or 'EXEC__CISO'
+    input_arg = raw_arg.strip() or DEFAULT_PERSONA
 
     try:
         persona_type = Schema__Persona__Types[input_arg]
@@ -24,4 +28,4 @@ if __name__ == '__main__':
 
         flow.execute_flow()
 
-        pprint(_.output)
+        pprint(_.output, width=200)
